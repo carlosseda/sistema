ce5793b8ea8007398a3b4fef5059d17a3a50f00d
xAbbadonx <manu69alluse58@gmail.com>
2020-10-09 11:55:34 +0200

    Modificación tabla automatización

--- manu/t_ram.ps1
+++ manu/t_ram.ps1
@@ -24,10 +24,51 @@ catch
     write-warning ("Could not open a connection to Database $MySQLDB on Host $MySQLHost. Error: "+$Error[0].ToString())
 }
 
+$brand = (Get-CIMInstance Win32_PhysicalMemory).Manufacturer
+$name = (Get-CIMInstance Win32_PhysicalMemory).Name
+$form_type = (Get-CIMInstance Win32_PhysicalMemory).FormFactor
+
+switch( $form_type )
+    {
+        8{
+            $form_type="DIMM" 
+        }
+        12{
+            $form_type="SODIMM"
+        }
+        default{
+            $form_type=""
+        }
+    }
+
+$type = (Get-CIMInstance Win32_PhysicalMemory).MemoryType
+
+switch( $type )
+    {
+        20{
+            $type="DDR1" 
+        }
+        21{
+            $type="DDR2"
+        }
+        24{
+            $type="DDR3"
+        }
+        26{
+            $type="DDR4"
+        }
+        default{
+            $type=""
+        }
+    }
+
+$size = (Get-CIMInstance Win32_PhysicalMemory).Capacity
+$max_voltage = (Get-CIMInstance Win32_PhysicalMemory).MaxVoltage
+$base_voltage = (Get-CIMInstance Win32_PhysicalMemory).MinVoltage
 
 $MYSQLCommand = New-Object MySql.Data.MySqlClient.MySqlCommand
 $MYSQLCommand.Connection = $connection
-$MYSQLCommand.CommandText='INSERT into `sistema`.`t_ram` (`brand`,`name`,`type`,`memory`,`cl`,`max_voltage`,`base_voltage`,`base_frequency`,`max_frequency`) VALUES("Kingston","HyperX","DDR4","16","16","1350","1200","2400","3466")'
+$MYSQLCommand.CommandText='INSERT into `sistema`.`t_ram` (`brand`,`name`,`form_type`,`type`,`size`,`cl`,`max_voltage`,`base_voltage`,`base_frequency`,`max_frequency`) VALUES("$($brand)","$($name)","$($form_type)","$($type)","$($size)","","$($max_voltage)","$($base_voltage)","","")'
 
 
 $Error.Clear()
