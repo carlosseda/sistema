6df19c84bef30d9336432d3a47692f5e87075eaa
AtomosEnAlmibar <jcmmnbvcxz@gmail.com>
2020-10-20 13:33:29 +0200

    mas cosas+

--- jon/computer.psm1
+++ jon/computer.psm1
@@ -1,40 +1,89 @@
 #!/usr/bin/pwsh
 
-class Computer 
-{
-    [string]$Reference
-    [string]$Type
-    [datetime]$StartUse
-    [datetime]$EndUse    
-    [int]$Active
+class computer {
+    [string]$reference
+    [string]$type
+    [DateTime]$startUse
+    [DateTime]$endUse        
+    [int]$markerId
+    [int]$placeId
+    [int]$spaceId
+    [int]$tableId
+    [int]$active
 
-    Computer()
-    {
-        $this.Active = 1
+    computer() {
+        $this.active = 1
     }
 
-    [string] getReference()
-    {
-        return $this.Reference
+    [string] getReference() {
+        return $this.reference
     }
 
-    [string] getType()
-    {
-        return $this.Type
+    [string] getType() {
+        return $this.type
     }
 
-    [datetime] getStartUse()
-    {
-        return $this.MaxMemorySize
+    [datetime] getStartUse() {
+        return $this.startUse
     }
 
-    [datetime] getEndUse()
-    {
-        return $this.MaxMemorySlots
+    [datetime] getEndUse() {
+        return $this.endUse
     }
 
-    [int] getActive()
-    {
-        return $this.Active
+    [int] getMarkerId() {
+        return $this.markerId
+    }
+
+    [int] getPlaceId() {
+        return $this.placeId
+    }
+
+    [int] getSpaceId() {
+        return $this.spaceId
+    }
+
+    [int] getTableId() {
+        return $this.markerId
+    }
+
+    [int] getActive() {
+        return $this.active
+    }
+
+    [string] setReference($reference) {
+        $this.reference = $reference
+    }
+
+    [string] setType($type) {
+        $this.type = $type
+    }
+
+    [datetime] setStartUse($startUse) {
+        $this.startUse = $startUse
+    }
+
+    [datetime] setEndUse($endUse) {
+        $this.endUse = $endUse
+    }
+
+    [int] setMarkerId($markerId) {
+        $this.markerId = $markerId
+    }
+
+    [int] setPlaceId($placeId) {
+        $this.placeId = $placeId
+    }
+
+    [int] setSpaceId($spaceId) {
+        $this.spaceId = $spaceId
+    }
+
+    [int] setTableId($tableId) {
+        $this.tableId = $tableId
+    }
+
+    [int] setActive($active) {
+        $this.active = $active
     }
 }
